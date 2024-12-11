8f68037ff710e4a00aa3a8f383bfa98da6b31088
bemunozm <be.munozmoya@gmail.com>
2024-12-11 13:43:55 -0300

    FEATURE 88: Implementacion de descuentos y fix varios

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -58,7 +58,6 @@ export class OrderService {
             status: status || 'Sin Pagar',
             type: type
         });
-        console.log('order', order);
         const savedOrder = await order.save();
         console.log('savedOrder', savedOrder);
 
